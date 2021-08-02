1cd3c881aa4d322f1348ad3e8ef8ef625928a4ed
tomoki <tomoki_1230@yahoo.co.jp>
2021-08-02 23:17:53 +0900

    アクションの追加とコントローラの作成をし、ゲストログイン処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   root "texts#index"
   resources :texts, only: [:index, :show]
   resources :movies, only: :index
