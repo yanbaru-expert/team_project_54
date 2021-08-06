982b3b6fc18e1247a5620ab44b778be22be59e96
阿部豊 <k1819abe@gmail.com>
2021-08-06 11:40:03 +0900

    2ページ目以降のレベルを連番表示

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
+  PER_PAGE = 12
   def index
     @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    @movies = Movie.page(params[:page]).per(PER_PAGE)
   end
 end
