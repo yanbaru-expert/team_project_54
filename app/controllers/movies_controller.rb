b35adcdd344219bdc96599bd0098acbb6fde8d29
gropmor <rari_atto@yahoo.co.jp>
2021-08-12 22:33:12 +0900

    PHP動画教材ページで「PHPの動画教材のみ」が表示されるように修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   PER_PAGE = 12
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+    @movies = Movie.where(genre: Movie.genre_list(params[:genre])).page(params[:page]).per(PER_PAGE)
   end
 end
