fce176f8ab8954ebd0c15ca76ccf216d00dfbed1
gropmor <rari_atto@yahoo.co.jp>
2021-08-14 19:40:49 +0900

    読破済み機能に使用する ReadProgress モデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :watch_progresses, dependent: :destroy
+  has_many :read_progresses, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
