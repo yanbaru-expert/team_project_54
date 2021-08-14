a2a4ace6cb784f70309fee1fdaff9ff74241b64b
gropmor <rari_atto@yahoo.co.jp>
2021-08-14 21:44:36 +0900

    テキスト教材一覧ページに読破済み機能を実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,6 @@
 class TextsController < ApplicationController
   def index
-    @texts = Text.where(genre: Text::RAILS_GENRE_LIST)
+    @texts = Text.where(genre: Text::RAILS_GENRE_LIST).includes(:read_progresses)
   end
 
   def show; end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,10 @@
 class Text < ApplicationRecord
   has_many :read_progresses, dependent: :destroy
 
+  def read_progressed_by?(user)
+    read_progresses.any? { |read_progress| read_progress.user_id == user.id }
+  end
+
   with_options presence: true do
     validates :genre
     validates :title
